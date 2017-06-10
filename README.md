49d8561e11a4513ebbdd74b458f5bbafe05c1b33
Dirk <bremendirk@gmail.com>
2017-06-10 18:08:23 +0100

    Refactored a bit, removed linebreaks and added author to subtitle

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Wox.Plugins.RandomQuote
 Wox plugin to pull off random quotes using https://forismatic.com/en/api/
 
+# Installation:
+from wox: wpm install RandomQuote
+
 ![screenshot](screen.png?raw=true "Screenshot")
--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 import requests
-import textwrap
 from wox import Wox
 
 icons_dir = './icons/'
+class Main(Wox):
+    def request(self, url):
+        if self.proxy and self.proxy.get("enabled") and self.proxy.get("server"):
+            proxies = {
+                "http": "http://{}:{}".format(self.proxy.get("server"), self.proxy.get("port")),
+                "https": "http://{}:{}".format(self.proxy.get("server"), self.proxy.get("port"))}
+            return requests.get(url, proxies=proxies)
+        else:
+            return requests.get(url)
 
-def randomquote(query):
-    results = []
-    send_data = {
-        'method': 'getQuote',
-        'format': 'json',
-        'lang': 'en'}
-    r = requests.post('http://api.forismatic.com/api/1.0/', send_data).json()
-    # alternatives:
-    # https://talaikis.com/api/quotes/random/
-    # https://favqs.com/api/qotd
-    currquote = textwrap.fill(r['quoteText'], width=90)
-    author = r['quoteAuthor']
-    if not author:
-        author = "Anonymous"
-    print()
-    results.append({
-        "Title": currquote + " ~ " + author,
-        "SubTitle": '',
-        "IcoPath": icons_dir + "quote.png"
-    })
-    if not results:
+    def query(self, query):
+        results = []
+        send_data = {
+            'method': 'getQuote',
+            'format': 'json',
+            'lang': 'en'}
+        r = requests.post('http://api.forismatic.com/api/1.0/', send_data).json()
+        # alternatives:
+        # https://talaikis.com/api/quotes/random/
+        # https://favqs.com/api/qotd
+        # currquote = textwrap.fill(r['quoteText'], width=90)
+        currquote = r['quoteText']
+        author = r['quoteAuthor']
+        if not author:
+            author = "Anonymous"
+        print()
         results.append({
-            "Title": 'Not found',
-            "SubTitle": '',
+            "Title": currquote,
+            "SubTitle": author,
             "IcoPath": icons_dir + "quote.png"
         })
-    return results
-
-class RandomQuote(Wox):
-    def query(self, query):
-        return randomquote(query)
-
+        if not results:
+            results.append({
+                "Title": 'Not found',
+                "SubTitle": '',
+                "IcoPath": icons_dir + "quote.png"
+            })
+        return results
 
 if __name__ == "__main__":
-    RandomQuote()
+    Main()
